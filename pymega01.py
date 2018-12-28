f87ffd1bd7be3d60accc9bea0c4264f351c711e5
Maurilio Silva <Mau@MacBook-Air-de-Maurilio.local>
2018-12-28 02:03:48 -0200

    Ajuste final

--- pymega01.py
+++ pymega01.py
@@ -7,13 +7,19 @@ q1 = [1,2,3,4,5,11,12,13,14,15,21,22,23,24,25]
 q2 = [6,7,8,9,10,16,17,18,19,20,26,27,28,29,30]
 q3 = [31,32,33,34,35,41,42,43,44,45,51,52,53,54,55]
 q4 = [36,37,38,39,40,46,47,48,49,50,56,57,58,59,60]
-Os10MaisSorteados = [10,5,53,4,23,33,54,24,51,42]
-Os10MaisAtrasados = [55,42,26,7,25,21,47,53,23,32]
 
-numapostas = 10
+Os10MaisSorteados = [10,5,53,4,23,33,54,24,51,42,17,37,27,52,13,28,56,6,30,32,34,43,16,2,36,44,29,50,18]
+Os10MaisAtrasados = [41,35,8,38,12,11,49,59,1,45,58,40,47,20,46,31,7,19,39,3,14,60,25,48,15,57,22,9,55,21,26]
+todos = [1,2,3,4,5,11,12,13,14,15,21,22,23,24,25,6,7,8,9,10,16,17,18,19,20,26,27,28,29,30,31,32,33,34,35,41,42,43,44,45,51,52,53,54,55,36,37,38,39,40,46,47,48,49,50,56,57,58,59,60]
+
+#Os10MaisSorteados = todos
+#Os10MaisAtrasados = todos
+
+numapostas = 60000000
 dezenas = 7
 apostas = []
 concursos = {}
+corteiguais = 3
 
 
 def CarregaConcursos():
@@ -39,14 +45,17 @@ def CarregaConcursos():
 
 def ValidaDezenas():
     iguais = []
+    retorno = True
     for concurso,dez in concursos.items():
         for d in dez:
             for i in apostas:
                 if d == i:
                     iguais.append(d)
-        if len(iguais) > 2:
-            print('Dezenas: {} saiu no concurso {}'.format(iguais,concurso))
+        if len(iguais) >= corteiguais:
+            #print('Dezenas: {} saiu no concurso {}'.format(iguais,concurso))
+            retorno = False
         del iguais[:]
+    return retorno
 
 
 
@@ -94,13 +103,17 @@ def apostar():
     sorteiaSobra(sobrou)
 
 def principal():
+    Validado = False
     CarregaConcursos()
     for i in range(1, numapostas+1):
         #time.sleep(5)
         apostar()
         apostas.sort()
-        print('minhas apostas {}: {}'.format(i, apostas))
-        ValidaDezenas()
+        # print('minhas apostas {}: {}'.format(i, apostas))
+        # ValidaDezenas()
+        Validado = ValidaDezenas()
+        if Validado:
+            print('>>>> Aposta valida {}: {}'.format(i, apostas))
         del apostas[:]
 
 principal()
